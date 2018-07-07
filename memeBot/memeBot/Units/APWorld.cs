c14c36c078d4e46bb6d9b225aafa5b8a95be128f
Metastable1883 <boson2003@gmail.com>
2018-07-07 07:00:02 -0700

    added frc and AP World cmd

---
+++ memeBot/memeBot/Units/APWorld.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memeBot.Units
+{
+    public class APWorld : ModuleBase<SocketCommandContext>
+    {
+        
+        [Command("APWorld")]
+        public async Task PingAsync() {
+            await ReplyAsync("https://www.longbranch.k12.nj.us/cms/lib/NJ01001766/Centricity/Domain/635/AP%20World%20History/The%20Earth%20and%20its%20Peoples%203rd%20Edition%20Online%20Textbook.pdf");
+            
+        }
+    }
+}
+
---
+++ memeBot/memeBot/Units/frc.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memeBot.Units
+{
+    public class frc : ModuleBase<SocketCommandContext>
+    {
+        [Command("frc")]
+        public async Task frcAsync([Remainder] string echo)
+        {
+            await ReplyAsync("https://www.thebluealliance.com/team/"+echo);
+              
+            
+
+        }
+        [Command("frc")]
+        public async Task PingAsync() {
+            await ReplyAsync("`You Must Enter a team number`");
+            
+        }
+    }
+}
+
