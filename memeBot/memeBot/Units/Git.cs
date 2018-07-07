e45d5311cb2e0150d48e0c12cddcf6aef12437a3
Metastable1883 <boson2003@gmail.com>
2018-07-07 11:43:22 -0700

    memed alot

---
+++ memeBot/memeBot/Units/Git.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memeBot.Units
+{
+    public class git : ModuleBase<SocketCommandContext>
+    {
+        [Command("git")]
+        public async Task frcAsync([Remainder] string echo)
+        {
+            await ReplyAsync("https://github.com/" + echo);
+              
+            
+
+        }
+        [Command("git")]
+        public async Task PingAsync() {
+            await ReplyAsync("`You Must Enter a valid user`");
+            
+        }
+    }
+}
+
---
+++ memeBot/memeBot/Units/help.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memeBot.Units
+{
+    public class Help : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task HelpAsync()
+        {
+               
+            await ReplyAsync("`ping [userID]`"+" Gets Someone`s attention");
+            await ReplyAsync("`APWorld`" + " provides textbook");
+            await ReplyAsync("`frc [team number]`" + " provides TBA info on team");
+            await ReplyAsync("`git [username]`" + " returns info on github user");
+            await ReplyAsync("`rekt [userID]`" + " generates a murderous statement");
+            await ReplyAsync("`roast [userID]`" + " roasts a user");
+            await ReplyAsync("`grantquote`" + "provides a quote from the legendary Mr. Grant");
+
+
+
+
+
+        }
+        
+    }
+}
+
